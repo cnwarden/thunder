bfc917145e7d6a07c050c93abce19084583fd80c
cnwarden <cnwarden@gmail.com>
2015-08-03 22:16:56 +0800

    + port changed to 80 + index page

--- bin/www.js
+++ bin/www.js
@@ -4,12 +4,12 @@ var http = require('http');
 var app = require('../app');
 var debug = require('debug')('thunder::server');
 
-var port = 3000;
+var port = 80;
 app.set('port', port);
 
 var server = http.createServer(app);
 
-server.listen(3000);
+server.listen(port);
 server.on('error', onError);
 
 function onError(error){
