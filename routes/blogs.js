6b0ddf596fcfbdbf3039704e3fdd4e41c374e572
cnwarden <cnwarden@gmail.com>
2015-08-06 17:44:25 +0800

    + some style changed

--- routes/blogs.js
+++ routes/blogs.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('blogs', {
         title:'Thunder_Blogs', 
-        blog_title:"Fox News announces participants in first 2016 GOP debate",
+        blog_title:"Fox News announces 习近平改革逻辑解读：重用改革促进派",
         blog_postdate: 'Aug-10-2015'
     });
 });
--- routes/hdata.js
+++ routes/hdata.js
@@ -2,12 +2,10 @@
 
 var express = require('express');
 var router = express.Router();
-var inst = require('../db/mongo')
+// var inst = require('../db/mongo')
 
 router.get('/', function(req, res, next) {
-    inst.find({}, function(err, docs){
-        res.render('hdata', {title:'Thunder', instruments: docs});
-    });
+    res.render('hdata', {title:'Thunder'});
 });
 
 module.exports = router;
